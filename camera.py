bbb90c1d358291db068a3a01abb274692298df46
Christoph Nölke <connium@christoph-noelke.de>
2018-08-12 17:22:53 +0200

    adjust .gitignore, add camera-config.yaml, take a single picture without countdown

--- camera.py
+++ camera.py
@@ -67,9 +67,6 @@ try:
     # Each of the following varibles, is now configured within [camera-config.yaml]:
     CAMERA_BUTTON_PIN = CONFIG['CAMERA_BUTTON_PIN']
     EXIT_BUTTON_PIN = CONFIG['EXIT_BUTTON_PIN']
-    TOTAL_PICS = CONFIG['TOTAL_PICS']
-    PREP_DELAY = CONFIG['PREP_DELAY']
-    COUNTDOWN = CONFIG['COUNTDOWN']
     PHOTO_W = CONFIG['PHOTO_W']
     PHOTO_H = CONFIG['PHOTO_H']
     SCREEN_W = CONFIG['SCREEN_W']
@@ -90,12 +87,6 @@ except KeyError as exc:
     sys.exit()
 
 #Optional config
-COPY_IMAGES_TO = []
-try:
-    if isinstance(CONFIG["COPY_IMAGES_TO"], list):
-        COPY_IMAGES_TO.extend( CONFIG["COPY_IMAGES_TO"] )
-    else:
-        COPY_IMAGES_TO.append( CONFIG["COPY_IMAGES_TO"] )
 
 except KeyError as exc:
     pass
@@ -119,7 +110,6 @@ CAMERA.hflip = CAMERA_HFLIP
 ########################
 def health_test_required_folders():
     folders_list=[SAVE_RAW_IMAGES_FOLDER]
-    folders_list.extend(COPY_IMAGES_TO)
     folders_checked=[]
 
     for folder in folders_list:
@@ -132,13 +122,6 @@ def health_test_required_folders():
         if not os.path.exists(folder):
             print('Creating folder: ' + folder)
             os.makedirs(folder)
-
-def print_overlay(string_to_print):
-    """
-    Writes a string to both [i] the console, and [ii] CAMERA.annotate_text
-    """
-    print(string_to_print)
-    CAMERA.annotate_text = string_to_print
 
 def get_base_filename_for_images():
     """
@@ -159,13 +142,6 @@ def get_base_filename_for_images():
     base_filepath = REAL_PATH + '/' + SAVE_RAW_IMAGES_FOLDER + '/' + base_filename
 
     return base_filepath
-
-def remove_overlay(overlay_id):
-    """
-    If there is an overlay, remove it
-    """
-    if overlay_id != -1:
-        CAMERA.remove_overlay(overlay_id)
 
 # overlay one image on screen
 def overlay_image(image_path, duration=0, layer=3, mode='RGB'):
@@ -224,61 +200,22 @@ def overlay_image(image_path, duration=0, layer=3, mode='RGB'):
 ###############
 ### Screens ###
 ###############
-def prep_for_photo_screen(photo_number):
-    """
-    Prompt the user to get ready for the next photo
-    """
-
-    #Get ready for the next photo
-    get_ready_image = REAL_PATH + '/assets/get_ready_' + str(photo_number) + '.png'
-    overlay_image(get_ready_image, PREP_DELAY, 3, 'RGBA')
-
-def taking_photo(photo_number, filename_prefix):
+def taking_photo(filename_prefix):
     """
     This function captures the photo
     """
 
     #get filename to use
-    filename = filename_prefix + '_' + str(photo_number) + 'of'+ str(TOTAL_PICS)+'.jpg'
-
-    #countdown from 3, and display countdown on screen
-    for counter in range(COUNTDOWN, 0, -1):
-        print_overlay("             ..." + str(counter))
-        sleep(1)
+    filename = filename_prefix + '.jpg'
 
     #Take still
     CAMERA.annotate_text = ''
     CAMERA.capture(filename)
-    print('Photo (' + str(photo_number) + ') saved: ' + filename)
-    return filename
+    print('Photo saved: ' + filename)
 
-def playback_screen(filename_prefix):
-    """
-    Final screen before main loop restarts
-    """
-
-    #Processing
-    print('Processing...')
-    processing_image = REAL_PATH + '/assets/processing.png'
-    overlay_image(processing_image, 2)
-
-    #Playback
-    prev_overlay = False
-    for photo_number in range(1, TOTAL_PICS + 1):
-        filename = filename_prefix + '_' + str(photo_number) + 'of'+ str(TOTAL_PICS)+'.jpg'
-        this_overlay = overlay_image(filename, False, (3 + TOTAL_PICS))
-        # The idea here, is only remove the previous overlay after a new overlay is added.
-        if prev_overlay:
-            remove_overlay(prev_overlay)
-        sleep(2)
-        prev_overlay = this_overlay
-
-    remove_overlay(prev_overlay)
-
-    #All done
     print('All done!')
     finished_image = REAL_PATH + '/assets/all_done_delayed_upload.png'
-    overlay_image(finished_image, 5)
+    overlay_image(finished_image, 2)
 
 def main():
     """
@@ -301,13 +238,9 @@ def main():
 
     #Display intro screen
     intro_image_1 = REAL_PATH + '/assets/intro_1.png'
-    intro_image_2 = REAL_PATH + '/assets/intro_2.png'
-    overlay_1 = overlay_image(intro_image_1, 0, 3)
-    overlay_2 = overlay_image(intro_image_2, 0, 4)
+    overlay_image(intro_image_1, 3)
 
     #Wait for someone to push the button
-    i = 0
-    blink_speed = 10
 
    #Use falling edge detection to see if button is being pushed in
     GPIO.add_event_detect(CAMERA_BUTTON_PIN, GPIO.FALLING)
@@ -335,15 +268,6 @@ def main():
 
         #Stay inside loop, until button is pressed
         if photo_button_is_pressed is None:
-
-            #After every 10 cycles, alternate the overlay
-            i = i+1
-            if i == blink_speed:
-                overlay_2.alpha = 255
-            elif i == (2 * blink_speed):
-                overlay_2.alpha = 0
-                i = 0
-
             #Regardless, restart loop
             sleep(0.1)
             continue
@@ -357,31 +281,14 @@ def main():
 
         #Get filenames for images
         filename_prefix = get_base_filename_for_images()
-        remove_overlay(overlay_2)
-        remove_overlay(overlay_1)
 
-        photo_filenames = []
-        for photo_number in range(1, TOTAL_PICS + 1):
-            prep_for_photo_screen(photo_number)
-            fname = taking_photo(photo_number, filename_prefix)
-            photo_filenames.append(fname)
-
-        #thanks for playing
-        playback_screen(filename_prefix)
-
-        #Save photos into additional folders (for post-processing/backup... etc.)
-        for dest in COPY_IMAGES_TO:
-            for src in photo_filenames:
-                print(src + ' -> ' + dest)
-                copy2(src, dest)
+        taking_photo(filename_prefix)
 
         # If we were doing a test run, exit here.
         if TESTMODE_AUTOPRESS_BUTTON:
             break
 
         # Otherwise, display intro screen again
-        overlay_1 = overlay_image(intro_image_1, 0, 3)
-        overlay_2 = overlay_image(intro_image_2, 0, 4)
         GPIO.add_event_detect(CAMERA_BUTTON_PIN, GPIO.FALLING)
         GPIO.add_event_detect(EXIT_BUTTON_PIN, GPIO.FALLING)
         print('Press the button to take a photo')
